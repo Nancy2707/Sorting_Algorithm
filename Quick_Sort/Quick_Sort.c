d6e22eda21361cd8f9596429dbe681ba63901f41
Nancy Sharma <nancysharma2000a@gmail.com>
2024-05-09 18:54:31 +0530

    Create Quick_sort.c

---
+++ Quick_Sort/Quick_Sort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+void swap(int* p1, int* p2)
+{
+    int temp;
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high; j++) {
+     	if (arr[j] < pivot) {
+            i++;
+             swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+   if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = { 10, 7, 8, 9, 1, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+  
+    quickSort(arr, 0, n - 1);
+  
+    printf("Sorted Array\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
